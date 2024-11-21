9e1b448a8b3b09aee627b5990c3847fd614fdf87
BrukAbebe <birukgalk@gmail.com>
2024-11-22 01:34:35 +0300

    fixing database parser

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
     console.log('MongoDB connected');
   } catch (error) {
