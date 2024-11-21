b2997305c2caf035bf4c37c74770ff5e62acc990
BrukAbebe <birukgalk@gmail.com>
2024-11-22 02:02:38 +0300

    add backend url

--- frontend/src/api/questionApi.js
+++ frontend/src/api/questionApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const questionApi = axios.create({
-  baseURL: "http://localhost:5000/api/",
+  baseURL: "https://q-aplatform-mauve.vercel.app/api/",
 });
 
 questionApi.interceptors.request.use(
